f8860d8c707d780c3b6e044fc5a06f4a99c42493
Max Gomes <maxgomes92@live.com>
2021-03-15 12:30:11 +0100

    Add basic list

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,31 @@
+import { useState, useRef } from 'react'
+
 function App() {
+  const [animals, setAnimals] = useState(['Dog'])
+  const nameInputRef = useRef()
+
+  const onClick = (event) => {
+    const form = event.target
+    event.preventDefault()
+
+    setAnimals([ ...animals, nameInputRef.current.value ]) // Spread operator
+    form.reset()
+  }
+
   return (
     <>
-      we got nothing here
+      <header>List of animals</header>
+      <form onSubmit={onClick}>
+        <label htmlFor='name'>Animal name</label>
+        <input id='name' placeholder="Type in animal name" ref={nameInputRef}></input>
+        <button type="submit">Click to add animal</button>
+      </form>
+
+      {animals.map((animal) => {
+        return (
+          <p>{animal}</p>
+        )
+      })}
     </>
   );
 }
