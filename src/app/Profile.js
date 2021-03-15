25072b0f32a6b788698cf75628c87e854e228cb3
Max Gomes <maxgomes92@live.com>
2021-03-15 10:18:23 +0100

    Task 1: Change name to first name and add last name input field

--- src/app/Profile.js
+++ src/app/Profile.js
@@ -12,7 +12,14 @@ export default function Profile () {
 
       <div className="profile-data">
         <div className="profile-data-item">
-          <h3>Name:</h3>
+          <h3>First name:</h3>
+          <p>...</p>
+        </div>
+
+        <VerticalSpace height={15} />
+
+        <div className="profile-data-item">
+          <h3>Last name:</h3>
           <p>...</p>
         </div>
 
